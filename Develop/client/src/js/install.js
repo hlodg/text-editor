b1f725f50e82252a5cb9c453d30000646b3ee891
Hannah Lodge <lodg0021@umn.edu>
2022-05-19 11:33:45 -0500

    finished creating this app! It works!

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,25 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const newevent = window.deferredPrompt;
+
+    if (!newevent){
+        return;
+    }
+
+    newevent.prompt();
+
+    window.deferredPrompt= null;
+    
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null
+});
